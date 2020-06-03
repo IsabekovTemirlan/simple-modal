da7048da7fb91620eb38b6607f1059ee8b1d3670
IsabekovTemirlan <isabekovtemirlan@gmail.com>
2020-06-03 16:08:35 +0300

    integreat modal in game

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 //////////////////////////////////////////
 //modal
 /////////////////////////////////////////
-const getEl = (el) => document.getElementById(el);
+const $ = (el) => document.getElementById(el);
 
-const show = getEl('show'),
- 	modal = getEl('modal'),
-	close = getEl('close');
+const modal = $('modal');
+const modalW = $('modal__window');
 
-const setVis = (el, style) => el.style.display = style;
+function modalWindow (title) {
+	const modalBody = `
+		<div class="modal__title">
+			<h1>Congratulations!</h1>
+			<hr>
+		</div>
+		<h1 class="title">${title}</h1>		
+		<div class="modal__buttons">
+			<hr>
+			<button id="close">Close</button>
+		</div>
+	`;
+	modal.innerHTML = modalBody;
+	modalW.style.display = 'flex';
+	modal.style.display = 'block';
 
-const showElem = (node, p1, p2) => {
-	node.addEventListener('click', () => {
-		setVis(modal, p1);
-		setVis(show, p2);
-	});
-}
+	const close = $('close');
 
-showElem(show, 'block', 'none');
-showElem(close, 'none', 'block');
+	if(close) {
+		close.addEventListener('click',() => {
+			modal.style.display, modalW.style.display = 'none';
+		});
+	}
+};
+
 
 ///////////////////////////////////////////
 //tic-tac-toe
@@ -25,22 +38,22 @@ showElem(close, 'none', 'block');
 //
 
 var ceil = document.getElementsByClassName("box-item"),
-	reset = document.getElementById("reset-game"),
-	message = document.getElementById("message")
+reset = document.getElementById("reset-game"),
+message = document.getElementById("message")
 
 stepCount = 0//нач.счёт
 player = "X"//текущий игрок
 x_player = [];
 y_player = [];
 const victory = [
-	[1, 2, 3],
-	[1, 4, 7],
-	[1, 5, 9],
-	[2, 5, 8],
-	[3, 6, 9],
-	[3, 5, 7],
-	[4, 5, 6],
-	[7, 8, 9]
+[1, 2, 3],
+[1, 4, 7],
+[1, 5, 9],
+[2, 5, 8],
+[3, 6, 9],
+[3, 5, 7],
+[4, 5, 6],
+[7, 8, 9]
 ];
 
 for (var i = 0; i < ceil.length; i++) {
@@ -53,23 +66,25 @@ function currentStep() {
 	if (!this.textContent) {//проверка содержании значении
 		this.innerText = player;
 		player === "X"
-			? x_player.push(num) && this.classList.add("x")
-			: y_player.push(num) && this.classList.add("o");
+		? x_player.push(num) && this.classList.add("x")
+		: y_player.push(num) && this.classList.add("o");
 		if (
 			(y_player.length > 2 || x_player.length > 2) &&
 			(checkWin(y_player, num) || checkWin(x_player, num))
-		) {
+			) {
 			// останавливаем ход игроков когда один из игроков выиграл
-			for (var i = 0; i < ceil.length; i++) {
-				ceil[i].removeEventListener("click", currentStep);
-			}
-			return (message.innerText = "Vinner player " + player);//обьявляем победителя
+		for (var i = 0; i < ceil.length; i++) {
+			ceil[i].removeEventListener("click", currentStep);
+		}
+			//return (message.innerText = "Vinner player " + player);//обьявляем победителя
+			let title = "Vinner player " + player;
+			return modalWindow(title);
 		}
 		changePlayer();
 		stepCount++;
 		stepCount === 9 //проверка ничьи
-			? (message.innerText = "Draw")
-			: (message.innerText = "Step player " + player);
+		? (message.innerText = "Draw")
+		: (message.innerText = "Step player " + player);
 	}
 }
 //Смена ходов игроков
@@ -96,7 +111,7 @@ reset.addEventListener("click", function() {
 function checkWin(arr, number) {
 	for (var w = 0, wLen = victory.length; w < wLen; w++) {//проверка массива
 		var someWinArr = victory[w],
-			count = 0;
+		count = 0;
 		if (someWinArr.indexOf(number) !== -1) { //проверям каж.массиве есть ли та цифра по которой кликнули
 			for (var k = 0, kLen = someWinArr.length; k < kLen; k++) {
 				if (arr.indexOf(someWinArr[k]) !== -1) {//проверям выигрышные комбинации в массиве
