c4a03d853ad9ce8ed61d01d5fbcb3dcfcea5146c
malikimansaparov <malikimansaparov@gmail.com>
2020-06-03 16:39:17 +0600

    ending game

--- index.js
+++ index.js
@@ -23,16 +23,15 @@ showElem(close, 'none', 'block');
 //tic-tac-toe
 ///////////////////////////////////////////
 //
-// const field = getEl('field');
+
 var ceil = document.getElementsByClassName("box-item"),
 	reset = document.getElementById("reset-game"),
 	message = document.getElementById("message")
 
-// first = true;
-
-stepCount = 0
-player = "X"
-
+stepCount = 0//нач.счёт
+player = "X"//текущий игрок
+x_player = [];
+y_player = [];
 const victory = [
 	[1, 2, 3],
 	[1, 4, 7],
@@ -43,58 +42,42 @@ const victory = [
 	[4, 5, 6],
 	[7, 8, 9]
 ];
-x_player = [];
-y_player = [];
 
-field.addEventListener('click', (e) => {
-	if(first) {
-		e.target.innerText = 'X';
-		x_player.push(+e.target.id);
-	}
-
-	if(!first) {
-		e.target.innerText = 'Y';
-		y_player.push(+e.target.id);		
-	}
-	first = !first;
-
-	if(x_player.length > 4) {
-		victory.map( v => (v[0] === x_player[0]) ? console.log('win') : null);
-	}
-});
 for (var i = 0; i < ceil.length; i++) {
 	ceil[i].addEventListener("click", currentStep);
 }
 
 function currentStep() {
+	// в перемменный num записываем значении id по которой кликнули
 	var num = +this.getAttribute("id");
-	if (!this.textContent) {
+	if (!this.textContent) {//проверка содержании значении
 		this.innerText = player;
 		player === "X"
 			? x_player.push(num) && this.classList.add("x")
-			: y_player.push(num) && this.classList.add("y");
+			: y_player.push(num) && this.classList.add("o");
 		if (
 			(y_player.length > 2 || x_player.length > 2) &&
 			(checkWin(y_player, num) || checkWin(x_player, num))
 		) {
+			// останавливаем ход игроков когда один из игроков выиграл
 			for (var i = 0; i < ceil.length; i++) {
 				ceil[i].removeEventListener("click", currentStep);
 			}
-			return (message.innerText = "Vinner player " + player);
+			return (message.innerText = "Vinner player " + player);//обьявляем победителя
 		}
 		changePlayer();
 		stepCount++;
-		stepCount === 9
+		stepCount === 9 //проверка ничьи
 			? (message.innerText = "Draw")
 			: (message.innerText = "Step player " + player);
 	}
 }
-
+//Смена ходов игроков
 function changePlayer()
 {
-	player === "X" ? (player = "Y") : (player = "X");
+	player === "X" ? (player = "O") : (player = "X");
 }
-
+//сброс счёта
 reset.addEventListener("click", function() {
 	for (var i = 0; i < ceil.length; i++) {
 		ceil[i].innerText = "";
@@ -106,17 +89,17 @@ reset.addEventListener("click", function() {
 	message.innerText = "Step player " + player;
 	for (var i = 0; i < ceil.length; i++) {
 		ceil[i].addEventListener("click", currentStep);
-		ceil[i].classList.remove("x", "y");
+		ceil[i].classList.remove("x", "o");//сброс фонового цвета
 	}
 });
-
+//смена игроков
 function checkWin(arr, number) {
-	for (var w = 0, wLen = victory.length; w < wLen; w++) {
+	for (var w = 0, wLen = victory.length; w < wLen; w++) {//проверка массива
 		var someWinArr = victory[w],
 			count = 0;
-		if (someWinArr.indexOf(number) !== -1) {
+		if (someWinArr.indexOf(number) !== -1) { //проверям каж.массиве есть ли та цифра по которой кликнули
 			for (var k = 0, kLen = someWinArr.length; k < kLen; k++) {
-				if (arr.indexOf(someWinArr[k]) !== -1) {
+				if (arr.indexOf(someWinArr[k]) !== -1) {//проверям выигрышные комбинации в массиве
 					count++;
 					if (count === 3) {
 						return true;
